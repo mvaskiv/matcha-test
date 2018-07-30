77abe37fec695ebb6e3ff7ca8cd1c899f8dd4fd8
mvaskiv <mike@vaskiv.com>
2018-07-30 17:13:40 +0300

    sqlconf

--- Matcha/app/Retchet/sqlconf.php
+++ Matcha/app/Retchet/sqlconf.php
@@ -1,6 +1,6 @@
 <?php
 
 return (array(
-'dsn' => 'mysql:host=localhost;dbname=matcha_db',
-'user' => 'root',
-'password' => 'qweqwe'));
+'dsn' => 'mysql:host=fdb21.awardspace.net;dbname=2700527_camagru',
+'user' => '2700527_camagru',
+'password' => 'qweqwe123'));
