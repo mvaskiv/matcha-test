20d4a1bcacd31c0faa84f3059137f00dd7674e94
mvaskiv <mike@vaskiv.com>
2018-08-02 20:51:43 +0300

    a

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,11 +13,11 @@ firebase.initializeApp(config);
 const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(payload => {
    const title = payload.notification.title;
-   console.log('payload', payload.notification.icon);
    const options = {
       body: payload.notification.body,
       icon: payload.notification.icon
    }
+   sendReceivedCallback(payload);
    return self.registration.showNotification(title, options);
 })
 
